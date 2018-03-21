a14fe26a3fcaa6ca5bb88d2a24d85762964d21fd
userblb <userblb@UNI002-L003-13.serlab.intra>
2018-03-21 10:54:23 -0300

    função de porcentagem adicionada

---
+++ porcentagemX/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+namespace ConsoleApp2
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+
+                Console.WriteLine("Digite o número que você deseja ");
+                
+                double VarA = Convert.ToDouble(Console.ReadLine());
+
+
+                Console.WriteLine("Digite a porcentagem ");
+                double VarB = Convert.ToDouble(Console.ReadLine());
+
+                double VarC;
+
+                VarB = VarB / 100;
+                VarC = VarB;
+                VarA = VarA * VarC;
+
+                Console.WriteLine("Resultado é: " + VarA);
+                Console.ReadLine();
+
+
+
+
+
+            }
+        }
+    }
+ 
+        
+    
