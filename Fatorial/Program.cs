bdd64b4644d08b6366ea78f56a1a0f7d69f065b1
userblb <userblb@UNI002-L003-14.serlab.intra>
2018-03-28 10:29:41 -0300

    fatorial adicionado

---
+++ Fatorial/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fatorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int number;
+
+            Console.WriteLine("Entre com o número que será fatorado:");
+            number = int.Parse(Console.ReadLine());
+
+            int fatorial;
+
+            fatorial = number;
+            for (int i = number - 1; i > 1; i--)
+            {
+                fatorial *= i;
+            }
+
+            Console.WriteLine("Valor de "+number +"! é: " + fatorial);
+
+            Console.Read();
+        }
+
+    }
+}
+
