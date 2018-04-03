2c6fa4b38501a64702bdb135b99eaa218a32873d
userblb <userblb@UNI002-L003-14.serlab.intra>
2018-04-03 12:36:11 -0300

    Dez porcento adicionado

--- Fatorial/Program.cs
+++ Fatorial/Program.cs
@@ -4,7 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-//Código para realização da função Fatorial
+//Código para realização da função Fatorial.
+
 namespace Fatorial
 {
     class Program
